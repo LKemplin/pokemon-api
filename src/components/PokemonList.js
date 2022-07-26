41e574a5d795ad2d2a35abbed19f1c657fa8de2c
Lynsey Kemplin <purplez479@gmail.com>
2022-07-25 18:28:34 -0700

    updated with axios

--- src/components/PokemonList.js
+++ src/components/PokemonList.js
@@ -1,16 +1,21 @@
 import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const PokemonList = () => {
     const [pokemon, setPokemon] = useState([])
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/?limit=807')
-            .then(response => response.json())
-            .then(response => setPokemon(response.results))
+        axios.get('https://pokeapi.co/api/v2/pokemon/?limit=807')
+            .then(response => {
+                console.log(response.data.results);
+                setPokemon(response.data.results);
+            })
+            .catch((err) => console.log(err))
     }, [])
 
   return (
     <div>
+        <h1>Look at all these pokemon!</h1>
         {pokemon.length > 0 && pokemon.map((poke, index) => {
             return ( <div key={index}>{poke.name}</div> )
         })}
